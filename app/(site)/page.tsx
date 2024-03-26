7c314387ece9f660441c4e4af51d8731a8073978
Denys Kolomiiets <106629418+DenysKo88@users.noreply.github.com>
2024-03-26 12:00:02 +0200

    + create Auth UI

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-
+import AuthForm from "./components/AuthForm";
 export default function Home() {
   return (
     <div className="flex flex-col min-h-full justify-center py-12 sm:px-6 lg:px-8 bg-gray-100">
@@ -15,7 +15,7 @@ export default function Home() {
           Sign in to your account
         </h2>
       </div>
-      {/*Auth form will be there */}
+      <AuthForm />
     </div>
   );
 }
